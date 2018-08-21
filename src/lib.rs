7ba3a64315b205cf80af34b2471f152a695d2407
tef <tef@twentygototen.org>
2018-08-21 04:41:57 +0100

    nice

--- src/lib.rs
+++ src/lib.rs
@@ -10,108 +10,117 @@ use std::mem;
 
 type Address = usize;
 
-//
-
-
-#[derive(Debug)]
-struct AtomicBox<T> {
-    ptr: AtomicPtr<T>,
-    _marker: PhantomData<T>,
+struct AtomicPtrVec<T> {
+    ptr: *mut AtomicPtr<T>,
+    len: AtomicUsize,
+    cap: usize,
 }
 
-impl<T> AtomicBox<T> {
-    pub fn new(b: Box<T>) -> AtomicBox<T> {
-        let uptr: usize = unsafe {  mem::transmute(Box::into_raw(b)) };
-        // let uptr = uptr|1;
-        let ptr: *mut T = unsafe { mem::transmute(uptr) };
-        AtomicBox {
-            ptr: AtomicPtr::new(ptr),
-            _marker: PhantomData,
+impl <T> AtomicPtrVec<T> {
+    pub fn new(size: usize) -> AtomicPtrVec<T> {
+        let mut v: Vec<*mut ()> = vec![ptr::null_mut(); size];
+        let mut v: Vec<AtomicPtr<T>> = unsafe { mem::transmute(v)};
+        let a = AtomicPtrVec {
+            ptr: v.as_mut_ptr(),
+            len: AtomicUsize::new(0),
+            cap: v.capacity(),
+        };
+        mem::forget(v);
+        a
+    }
+    pub fn append(&self, value: *mut T) -> Result<usize, ()> {
+        let index = self.len.fetch_add(1, Ordering::SeqCst);
+        if index >= self.cap {
+            panic!("out of bounds")
+        }
+        unsafe {
+            let ptr = &*self.ptr.offset(index as isize);
+            let old = ptr.compare_and_swap(ptr::null_mut(), value, Ordering::SeqCst);
+            if old.is_null() {
+                Ok(index)
+            } else {
+                Err(())
+            }
+        }
+    }
+    pub fn load(&self, index: usize) -> *mut T {
+        unsafe {
+            let ptr = &*self.ptr.offset(index as isize);
+            ptr.load(Ordering::SeqCst)
+        }
+    }
+    pub fn swap(&self, index: usize, new: *mut T) -> *mut T {
+        unsafe {
+            let ptr = &*self.ptr.offset(index as isize);
+            ptr.swap(new, Ordering::SeqCst)
         }
     }
 
-    pub fn load(&self) -> *mut T {
-        let uptr: usize = unsafe { mem::transmute(self.ptr.load(Ordering::SeqCst)) };
-        // let uptr = uptr & (!1 as usize);
-        let ptr: *mut T = unsafe { mem::transmute(uptr) };
-        ptr
-    }   
-
-    pub fn swap(&self, new: *mut T) -> *mut T {
-        self.ptr.swap(new, Ordering::SeqCst)
-    }
-
-    pub fn compare_and_swap(&self, old: *mut T, new: *mut T) -> Result<*mut T, *mut T> {
-        let r = self.ptr.compare_and_swap(old, new, Ordering::SeqCst);
-        if old == r {
-            Ok(r)
-        } else {
-            Err(r)
-        }
-    }
-
-}
-impl<T> Drop for AtomicBox<T> {
-    fn drop(&mut self) {
-        let p = self.ptr.swap(ptr::null_mut(), Ordering::SeqCst);
-        if p != ptr::null_mut() {
-            let uptr: usize = unsafe { mem::transmute(p)};
-            if uptr & 1 == 0 {
-                unsafe {Box::from_raw(p)};
+    pub fn compare_and_swap(&self, index: usize, old: *mut T, new: *mut T) -> Result<*mut T, *mut T> {
+        unsafe {
+            let ptr = &*self.ptr.offset(index as isize);
+            let out = ptr.compare_and_swap(old, new, Ordering::SeqCst);
+            if out == old {
+                Ok(out)
+            } else {
+                Err(out)
             }
         }
     }
 }
 
-pub struct AtomicVec<T> {
-    arr: Arc<Mutex<Vec<T>>>,
+
+struct CAS<T> {index: usize, old: *mut T, new: *mut T}
+
+enum State { Uncommitted, Committing, Committed, Cancelled }
+
+struct Descriptor<T> {
+    state: State,
+    operations: Vec<CAS<T>>,
 }
 
-impl <T> AtomicVec<T> {
-    pub fn with_capacity(capacity: usize) -> AtomicVec<T> {
-        AtomicVec {
-            arr: Arc::new(Mutex::new(Vec::with_capacity(capacity))),
+impl <T> Descriptor<T> {
+    fn new() -> Descriptor<T> {
+        let mut vec = Vec::with_capacity(8);
+        Descriptor {
+            state: State::Uncommitted,
+            operations: vec,
         }
     }
-    pub fn append(&self, value: T) -> usize {
-        let mut v = self.arr.lock().unwrap();
-        v.push(value);
-        v.len()
+    fn add(&mut self, index: usize, old: *mut T, new: *mut T) {
+        self.operations.push( 
+            CAS {index: index, old: old, new: new}
+        );
     }
-}
-
-
-
-enum Operations<P> {
-    Insert(Address, *mut P),
-    CompareAndSwap(Address, *mut P, *mut P),
 }
 
 pub struct Transaction<'a, P: 'a> {
-    heap: &'a SharedHeap<P>,
+    vec: &'a AtomicPtrVec<P>,
     epoch: usize,
-    operations: Vec<Operations<P>>,
-    // read_set
+    descriptor: *mut Descriptor<P>,
+    collector: &'a Collector<'a, P>,
 }
 
 
 impl <'a, P> Transaction<'a, P> {
-    pub fn borrow(&mut self, address:Address) -> &'a P {
-        let mut vec = self.heap.table.lock().unwrap();
-        let ab = vec.get(address).unwrap();
-        let ptr = ab.load();
-        let uptr: usize = unsafe { mem::transmute(ptr) };
-        unsafe { & *ptr}
+    pub fn fake_item(&self) -> *mut P {
+        let ptr: usize = unsafe { mem::transmute(self.descriptor) };
+        let ptr = ptr | 1;
+        unsafe{mem::transmute(self.descriptor)}
     }
-
+    pub fn borrow(&mut self, address:Address) -> &'a P {
+        let ptr = self.vec.load(address);
+        unsafe { &*ptr }
+    }
+    
     pub fn insert(&mut self, value: P) -> Address {
-        let b = Box::new(value);
-        let ab = AtomicBox::new(b);
-        let ptr = ab.load();
-        let mut vec = self.heap.table.lock().unwrap();
-        let addr = vec.len();
-        vec.push(ab);
-        self.operations.push(Operations::Insert(addr, ptr));
+        let ptr = Box::into_raw(Box::new(value));
+        let fake = self.fake_item();
+        let addr = self.vec.append(fake).unwrap();
+        unsafe {
+            let d = &mut *self.descriptor;
+            d.add(addr, fake, ptr);
+        }
         addr
     }
     
@@ -132,9 +141,16 @@ impl <'a, P> Transaction<'a, P> {
         // optimisitic has write list & if read set promotes it
         unimplemented!()
     }
-    pub fn apply<'b>(&mut self, session: &mut Session<'b,P>) -> bool {
+    pub fn apply(&mut self) -> bool {
+        unsafe {
+            let d = &mut *self.descriptor;
+            for x in &d.operations {
+                let result = self.vec.compare_and_swap(x.index, x.old, x.new);
+                
+                print!("apply {}", x.index);
+            }
+        }
         // in pessimistic, unlock each item and repl        ace it
-
         // in optimistic, lock each item, validate unchanged
         // return
         true
@@ -152,49 +168,50 @@ impl <'a, P> Transaction<'a, P> {
 impl<'a, T> Drop for Transaction<'a, T> {
     fn drop(&mut self) {
         // if !apply, cancel
-        ;
+        unsafe { Box::from_raw(self.descriptor) };
     }
 }
 
 struct Delete<P> {
     epoch: usize,
     address: Address,
-    value: P,
+    value: *mut P,
 }
 
-pub struct Session<'a, P: 'a> {
-    heap: &'a SharedHeap<P>,
+pub struct Collector<'a, P: 'a> {
+    heap: &'a AtomicHeap<P>,
     delete: Vec<Delete<P>>, // ArcMutex
     // read_set
 } 
 
-impl <'a, P> Session<'a, P> {
-    fn collect_later(&self, epoch: usize, value: *mut P) {
+impl <'a, P> Collector<'a, P> {
+    fn collect_later(&mut self, epoch: usize, address: Address, value: *mut P) {
+        self.delete.push(Delete{epoch: epoch, address: address, value: value});
     }
     pub fn collect(&mut self) {
-        // read current epoch, 
+        // read current epoch, scan list, prune
     }
 
 }
 
-impl<'a, T> Drop for Session<'a, T> {
+impl<'a, T> Drop for Collector<'a, T> {
     fn drop(&mut self) {
         // spin and collect ?
         ;
     }
 }
 
-pub struct SharedHeap<P> {
-    table: Mutex<Vec<AtomicBox<P>>>, // ArcMutex
+pub struct AtomicHeap<P> {
+    vec: AtomicPtrVec<P>,
     epoch: AtomicUsize,
 }
 
-impl <P> SharedHeap<P> {
-    pub fn new(capacity: usize) -> SharedHeap<P> {
-        let t = Mutex::new(Vec::with_capacity(capacity));
+impl <P> AtomicHeap<P> {
+    pub fn new(capacity: usize) -> AtomicHeap<P> {
+        let t = AtomicPtrVec::new(capacity);
         let e = AtomicUsize::new(0);
-        SharedHeap {
-            table: t,
+        AtomicHeap {
+            vec: t,
             epoch: e,
         }
     }
@@ -203,53 +220,54 @@ impl <P> SharedHeap<P> {
         self.epoch.load(Ordering::SeqCst)
     }
 
-    pub fn session<'a>(&'a self) -> Session<'a, P> {
+    pub fn collector<'a>(&'a self) -> Collector<'a, P> {
         let v = Vec::with_capacity(20);
-        Session {heap:self, delete: v}
+        Collector {heap:self, delete: v}
     }
-    pub fn transaction<'a>(&'a self) -> Transaction<'a, P> {
+    pub fn transaction<'a, 'b>(&'a self, collector: &'a mut Collector<'b, P>) -> Transaction<'a, P> {
+        let d = Box::new(Descriptor::new());
         Transaction {
-            heap: self,
+            vec: &self.vec,
             epoch: self.current_epoch(),
-            operations: Vec::with_capacity(10),
+            descriptor: Box::into_raw(d),
+            collector: collector,
         } 
     }
 
 }
 
-impl<T> Drop for SharedHeap<T> {
+impl<T> Drop for AtomicHeap<T> {
     fn drop(&mut self) {
-        // 
+        // to_vec() on atomicvecptr
+        // then iterate, panic on locked entry
+        // and then rebox and drop
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use SharedHeap;
+    use AtomicHeap;
+    use std::sync::atomic::{AtomicPtr, AtomicUsize, Ordering};
     use std::sync::Arc;
     use std::mem;
+    use std::ptr;
     #[test]
     fn it_works() {
-        let h = Arc::new(SharedHeap::new(1024));
-        let mut s = h.session();
+        let h = Arc::new(AtomicHeap::new(1024));
+        let mut s = h.collector();
         let mut addr = 0;
         {
-            let mut txn = h.transaction();
+            let mut txn = h.transaction(&mut s);
             addr = txn.insert("example".to_string());
-            txn.apply(&mut s);
+            txn.apply();
         }
         print!("two \n");
         {
-            let mut txn2 = h.transaction();
-            print!("nice\n");
+            let mut txn2 = h.transaction(&mut s);
             let o = txn2.borrow(addr);
-            print!("nice\n");
             let uptr: usize = unsafe {mem::transmute(o)};
-            print!("two: {}\n", uptr);
-        //       addr = txn2.insert("example2".to_string());
-            print!("exit");
+            print!("read: {}\n", uptr);
         }
-        print!("nice");
         s.collect();
     }
 }
